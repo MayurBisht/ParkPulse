98429b4d3fa112ebfb5dfe499ae093b5db0c04ab
Mayur Bisht <mayurbishtmb1@gmail.com>
2026-05-15 14:04:38 +0530

    deploy reaady

--- web_app.py
+++ web_app.py
@@ -1561,5 +1561,9 @@ def live_feed():
     )
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
